e4756f4fd126ae46b0c10cb236c63832e8a5fa0b
sc1991327 <sc1991327@gmail.com>
2016-05-19 09:59:02 +0800

    Basic Boardcast Scoket Server

---
+++ SculptorScoketServer/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SculptorScoketServer
+{
+    class Client
+    {
+        private Socket clientSocket;
+        private byte[] data = new byte[1024];
+
+        public Client(Socket s)
+        {
+            clientSocket = s;
+            // thread for each client
+            Thread thread = new Thread(ReceiveMessage);
+            thread.Start();
+        }
+
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                // client disconnect
+                //if (clientSocket.Poll(10, SelectMode.SelectRead))
+                //{
+                //    clientSocket.Close();
+                //    break;
+                //}
+
+                // send message
+                int length = clientSocket.Receive(data);
+                string message = Encoding.UTF8.GetString(data, 0, length);
+
+                // boardcast message
+                Program.BroadcastMessage(message);
+                //Console.WriteLine(message);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            clientSocket.Send(data);
+        }
+
+        public bool Connected()
+        {
+            return clientSocket.Connected;
+        }
+    }
+}
---
+++ SculptorScoketServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SculptorScoketServer
+{
+    class Program
+    {
+        private static string myIP = "10.32.93.177";
+        private static int myProt = 8885;
+
+        static List<Client> clientlists = new List<Client>();
+
+        public static void BroadcastMessage(string message)
+        {
+            var NotConnectClient = new List<Client>();
+            foreach (var client in clientlists)
+            {
+                if (client.Connected())
+                {
+                    client.SendMessage(message);
+                }
+                else
+                {
+                    NotConnectClient.Add(client);
+                }
+            }
+            foreach (var temp in NotConnectClient)
+            {
+                clientlists.Remove(temp);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse(myIP);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, myProt);
+            tcpServer.Bind(ipEndPoint);
+
+            Console.WriteLine("Server Start....");
+            tcpServer.Listen(10);
+
+            while (true)
+            {
+                Socket clientSocket = tcpServer.Accept();
+                Client client = new Client(clientSocket);
+                Console.WriteLine("Connect a Client...");
+                clientlists.Add(client);
+            }
+        }
+    }
+
+}
